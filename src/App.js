49d34bd3731c8f1bbe01c7300ab31453db6e5adf
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-19 19:30:50 -0600

    Changed ScoreBoardContext to ScoresContext and added a BottomRowContext.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./components/BottomRow";
 
-import ScoreBoardContext from './contexts/ScoreBoardContext';
+import ScoresContext from './contexts/ScoresContext';
+import BottomRowContext from './contexts/BottomRowContext';
 
 
 
@@ -11,6 +12,12 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+
+  const [downNumber, setDownNumber] = useState(1);
+  const [yardsToGo, setYardsToGo] = useState(0);
+  const [yardLine, setYardLine] = useState(1);
+  const [quarterNumber, setQuarterNumber] = useState(1);
+
 
   const homeTouchDown = (e) => {
     setHomeScore(homeScore + 7)
@@ -30,38 +37,40 @@ function App() {
 
 
   return (
-    <ScoreBoardContext.Provider value={{ homeScore, awayScore }}>
-  <div className="container">
-        <section className="scoreboard">
-          <div className="topRow">
-            <div className="home">
-              <h2 className="home__name">Lions</h2>
+    <ScoresContext.Provider value={{ homeScore, setHomeScore, awayScore, setAwayScore }}>
+      <BottomRowContext.Provider value={{  }}>
+        <div className="container">
+          <section className="scoreboard">
+            <div className="topRow">
+              <div className="home">
+                <h2 className="home__name">Lions</h2>
 
-              {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+                {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-              <div className="home__score">{homeScore}</div>
+                <div className="home__score">{homeScore}</div>
+              </div>
+              <div className="timer">00:03</div>
+              <div className="away">
+                <h2 className="away__name">Tigers</h2>
+                <div className="away__score">{awayScore}</div>
+              </div>
             </div>
-            <div className="timer">00:03</div>
-            <div className="away">
-              <h2 className="away__name">Tigers</h2>
-              <div className="away__score">{awayScore}</div>
+            <BottomRow />
+          </section>
+          <section className="buttons">
+            <div className="homeButtons">
+              {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+              <button className="homeButtons__touchdown" onClick={homeTouchDown}>Home Touchdown</button>
+              <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
             </div>
-          </div>
-          <BottomRow />
-        </section>
-        <section className="buttons">
-          <div className="homeButtons">
-            {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-            <button className="homeButtons__touchdown" onClick={homeTouchDown}>Home Touchdown</button>
-            <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
-          </div>
-          <div className="awayButtons">
-            <button className="awayButtons__touchdown" onClick={awayTouchDown}>Away Touchdown</button>
-            <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
-          </div>
-        </section>
-      </div>
-    </ScoreBoardContext.Provider>
+            <div className="awayButtons">
+              <button className="awayButtons__touchdown" onClick={awayTouchDown}>Away Touchdown</button>
+              <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
+            </div>
+          </section>
+        </div>
+      </BottomRowContext.Provider>
+    </ScoresContext.Provider>
     
   );
 }
---
+++ src/contexts/BottomRowContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const BottomRowContext = createContext();
+export default BottomRowContext;
