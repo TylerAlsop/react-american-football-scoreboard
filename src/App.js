444bc5602b64acd38a81ab5b889b7d8d2072ced0
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-19 19:14:27 -0600

    Created a ScoreBoardContext.js for state management. Imported it into App.js and wrapped the app in the Provider.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import BottomRow from "./components/BottomRow";
+
+import ScoreBoardContext from './contexts/ScoreBoardContext';
 
 
 
@@ -28,36 +30,39 @@ function App() {
 
 
   return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+    <ScoreBoardContext.Provider value={{ homeScore, awayScore }}>
+  <div className="container">
+        <section className="scoreboard">
+          <div className="topRow">
+            <div className="home">
+              <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+              {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homeScore}</div>
+              <div className="home__score">{homeScore}</div>
+            </div>
+            <div className="timer">00:03</div>
+            <div className="away">
+              <h2 className="away__name">Tigers</h2>
+              <div className="away__score">{awayScore}</div>
+            </div>
           </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
+          <BottomRow />
+        </section>
+        <section className="buttons">
+          <div className="homeButtons">
+            {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+            <button className="homeButtons__touchdown" onClick={homeTouchDown}>Home Touchdown</button>
+            <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
           </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={homeTouchDown}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={homeFieldGoal}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={awayTouchDown}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
-        </div>
-      </section>
-    </div>
+          <div className="awayButtons">
+            <button className="awayButtons__touchdown" onClick={awayTouchDown}>Away Touchdown</button>
+            <button className="awayButtons__fieldGoal" onClick={awayFieldGoal}>Away Field Goal</button>
+          </div>
+        </section>
+      </div>
+    </ScoreBoardContext.Provider>
+    
   );
 }
 
---
+++ src/contexts/ScoreBoardContext.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const ScoreBoardContext = createContext();
+export default ScoreBoardContext;
